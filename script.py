33671067a9e2e48b699fb46b711ac4116087dc84
DRome37 <dayroncid10@gmail.com>
2026-04-08 11:32:52 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,18 @@
+def main_menu():
+    print("\n--- Main Menu ---")
+    print("1. Start Game")
+    print("2. Exit")
+
+    choice = input("Select your choice: ")
+    if choice == "1":
+        return "Gameplay"
+    elif choice == "2":
+        return "Exit"
+        sys.exit()
+    else:
+        print("Invalid Choice")
+        return main_menu()
+
 playername=input("Hello, please enter your name: ")
 print("Hello", playername, "welcome to the adventure.")
 
