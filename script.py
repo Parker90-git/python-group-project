3cad43be79e3a4d7a0328ad61f5e12c710f0d659
Parker90-git <parkeradonahue@gmail.com>
2026-04-11 17:53:38 -0400

    Update script.py

--- script.py
+++ script.py
@@ -36,5 +36,66 @@ while True:
         break
     elif choice2 == "2":
         print("Too late.")
+    else:
+        print("Invalid Choice. Please try again.")
+
+print("\n---Goblin Encounter!---")
+print("A goblin approaches you, what do you do?")
+print("1. FIGHT")
+print("2. RUN")
+while True:
+    choice3=input("Select your choice: ")
+    if choice3 == "1":
+        print("You defeated the goblin!")
+        break
+    elif choice3 == "2":
+        print("The goblin struck while your back was turned!")
+        print("\n---GAME OVER---")
+        print("Returning you back to last save...")
+    else:
+        print("Invalid Choice. Please try again.")
+
+print("\n---Snake Encounter!---")
+print("Two snakes block your path!")
+print("1. Check first snake")
+print("2. Check second snake")
+while True:
+    choice4=input("Select your choice: ")
+    if choice4 == "1":
+        print("HEALTH: 10")
+        break
+    elif choice4 == "2":
+        print("HEALTH: 2")
+        break
+    else:
+        print("Invalid Choice. Please try again.")
+
+print("\nWhich snake will you attack?")
+print("1. First snake")
+print("2. Second snake")
+while True:
+    choice5=input("Select your choice: ")
+    if choice5 == "1":
+        print("It's unwise to prioritize the strongest!")
+        print("\n---GAME OVER---")
+        print("Returning you back to last save...")
+    elif choice5 == "2":
+        print("Wise choice! The weaker snake fell!")
+        break
+    else:
+        print("Invalid Choice. Please try again.")
+
+print("\nONE SNAKE REMAINS!")
+print("1. Attack")
+print("2. Retreat and move forward")
+while True:
+    choice6=input("Select your choice: ")
+    if choice6 == "1":
+        print("The snake was too tough!")
+        print("\n---GAME OVER---")
+        print("Returning you back to last save...")
+    elif choice6 == "2":
+        print("Wise choice! The forest is wide and open.")
+        break
 
 
