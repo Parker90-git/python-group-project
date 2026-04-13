6819593523509052f72a028829b86d0274eed9fa
DRome37 <dayroncid10@gmail.com>
2026-04-13 13:34:38 -0400

    Update script.py
    Changed menu to actually work

--- script.py
+++ script.py
@@ -1,27 +1,24 @@
 import sys
 
 def main_menu():
-        print("\n--- Main Menu ---")
-        print("1. Start Game")
-        print("2. Exit")
-
-        choice = input("Select your choice: ")
-
-        if choice == "1":
-            return "Gameplay"
-        elif choice == "2":
-            print("Exiting...")
-            sys.exit()
-        else:
-            print("Invalid Choice. Please try again.")
-            return "Menu"
+    print("\n--- Main Menu ---")
+    print("1. Start Game")
+    print("2. Exit")
+    choice = input("Select your choice: ")
+    return choice
 
 if __name__ == "__main__":
-    state = main_menu()
-    if state == "Gameplay":
-        print("Game will now start.")
-    else:
-        print(f"The menu ended with state: {state}")
+    while True:
+        state = main_menu()
+
+        if state == "1":
+            print("Game will now start.")
+            break
+        elif state == "2":
+            print("Exiting.")
+            sys.exit()
+        else:
+            print("Please enter a valid option.")
 
 playername=input("Hello, please enter your name: ")
 print("Hello", playername, "welcome to the adventure.")
